27227f879ece65bf9c07af6a531433fca6ed3a95
forfindo <3530766280@qq.com>
2026-08-22 19:25:02 +0800

    refactor: Flag is only used as a switch for related functions

---
+++ packages/openchat/__tests__/flag/index.test.ts
@@ -0,0 +1,26 @@
+import { describe, expect } from 'vitest';
+import { Flag, setFlag, setFlags } from '@/flag';
+
+describe('flag', () => {
+  it('setFlag', () => {
+    const r1 = setFlag('TEST_FLAG', 'test');
+    // @ts-ignore
+    const r2 = setFlag('CONFIG_DIR', 'd://');
+
+    expect(r1).toBeTruthy();
+    expect(r2).toBeFalsy();
+    expect(process.env['TEST_FLAG']).toBe('test');
+    expect(process.env['CONFIG_DIR']).toBe(void 0);
+    expect(Flag.CONFIG_DIR).toBe(void 0);
+  });
+
+  it('setFlags', () => {
+    setFlags({
+      CONFIG_DIR: 'test'
+    });
+    process.env['CONFIG_CONTENT'] = 'hhhhhhhhh';
+
+    expect(Flag.CONFIG_DIR).toBe('test');
+    expect(Flag.CONFIG_CONTENT).toBe(void 0);
+  });
+});
--- packages/openchat/src/flag/index.ts
+++ packages/openchat/src/flag/index.ts
@@ -3,71 +3,89 @@ import * as process from 'node:process';
 
 const log = Log.create();
 
-function truthy(key: string) {
+const truthy = (key: string) => {
   const value = process.env[key]?.toLowerCase();
   return value === 'true' || value === '1';
-}
+};
 
-function oneOf(...keys: string[]) {
+const some = (...keys: string[]) => {
   for (const key of keys) {
     if (truthy(key)) {
       return true;
     }
   }
   return false;
-}
-
-const StaticFlag = {
-  CONFIG_DIR: process.env['CONFIG_DIR'],
-  AUTH_CONTENT: process.env['AUTH_CONTENT'],
-  CONFIG_CONTENT: process.env['CONFIG_CONTENT'],
-  TEST_HOME: process.env['TEST_HOME'],
-  PERMISSION: process.env['PERMISSION'],
-  DISABLE_AUTO_COMPACT: process.env['DISABLE_AUTOCOMPACT'],
-  DISABLE_PRUNE: process.env['DISABLE_PRUNE'],
-  MODELS_URL: process.env['MODELS_URL'],
-  MODELS_PATH: process.env['MODELS_PATH'],
-  DISABLE_MODELS_FETCH: truthy('DISABLE_MODELS_FETCH'),
-  DISABLE_EXTERNAL_SKILLS: truthy('DISABLE_EXTERNAL_SKILLS'),
-  DISABLE_CLAUDE_CODE_SKILLS: oneOf('DISABLE_CLAUDE_CODE_SKILLS', 'DISABLE_CLAUDE_CODE'),
-  DISABLE_CLAUDE_CODE_PROMPT: oneOf('DISABLE_CLAUDE_CODE_PROMPT', 'DISABLE_CLAUDE_CODE')
 };
+
+const positiveInteger = (key: string) => {
+  const value = Number(process.env[key]);
+  return Number.isInteger(value) && value > 0 ? value : void 0;
+};
+
+const getStaticFlag = () => {
+  return {
+    CONFIG_DIR: process.env['CONFIG_DIR'],
+    AUTH_CONTENT: process.env['AUTH_CONTENT'],
+    CONFIG_CONTENT: process.env['CONFIG_CONTENT'],
+    TEST_HOME: process.env['TEST_HOME'],
+    PERMISSION: process.env['PERMISSION'],
+    DISABLE_AUTO_COMPACT: process.env['DISABLE_AUTOCOMPACT'],
+    DISABLE_PRUNE: process.env['DISABLE_PRUNE'],
+    MODELS_URL: process.env['MODELS_URL'],
+    MODELS_PATH: process.env['MODELS_PATH'],
+    DISABLE_MODELS_FETCH: truthy('DISABLE_MODELS_FETCH'),
+    DISABLE_EXTERNAL_SKILLS: truthy('DISABLE_EXTERNAL_SKILLS'),
+    DISABLE_CLAUDE_CODE_SKILLS: some('DISABLE_CLAUDE_CODE_SKILLS', 'DISABLE_CLAUDE_CODE'),
+    DISABLE_CLAUDE_CODE_PROMPT: some('DISABLE_CLAUDE_CODE_PROMPT', 'DISABLE_CLAUDE_CODE'),
+    bashDefaultTimeoutMs: positiveInteger('EXPERIMENTAL_BASH_DEFAULT_TIMEOUT_MS'),
+    GIT_BASH_PATH: process.env['GIT_BASH_PATH']
+  };
+};
+
+let StaticFlag = getStaticFlag();
 
 type STAKey = keyof typeof StaticFlag;
 
 export const Flag = new Proxy(
   {
-    // Static variable
+    // Static variable,
     ...StaticFlag,
     // Dynamic variable
-    get DISABLE_PROJECT_CONFIG() {
-      return truthy('DISABLE_PROJECT_CONFIG');
-    },
-    get ENABLE_EXPERIMENTAL_MODELS() {
-      return truthy('ENABLE_EXPERIMENTAL_MODELS');
-    }
+    DISABLE_PROJECT_CONFIG: truthy('DISABLE_PROJECT_CONFIG'),
+    ENABLE_EXPERIMENTAL_MODELS: truthy('ENABLE_EXPERIMENTAL_MODELS')
   },
   {
-    get(target, key, receiver): string | undefined {
-      if (Object.hasOwn(target, key)) {
-        return Reflect.get(target, key, receiver) as string | undefined;
+    get(target, key): string | undefined {
+      if (Object.hasOwn(StaticFlag, key)) {
+        return Reflect.get(StaticFlag, key) as string | undefined;
       }
       return process.env[key.toString()];
     },
-    set(target, key) {
+    set() {
       log.warn(
-        `Flag key: ${key.toString()} not recommended to modify. If you need to modify environment variables, please use setFlag`
+        `Method not allowed. If you need to modify environment variables, please use setEnv`
       );
       return false;
     }
   }
 );
 
-export type FlagKey = keyof typeof Flag;
+type Flag = typeof Flag;
+export type FlagKey = keyof Flag;
 
 export const setFlag = <T extends string>(
   key: Exclude<T, STAKey>,
   value: string | number | boolean
 ) => {
+  if (Object.hasOwn(StaticFlag, key)) {
+    log.warn(`property: ${key} not allowed to be modified`);
+    return false;
+  }
   process.env[key] = value.toString();
+  return true;
+};
+
+export const setFlags = (flag: Partial<Flag>) => {
+  Object.assign(process.env, flag);
+  StaticFlag = getStaticFlag();
 };
